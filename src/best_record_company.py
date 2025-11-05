67225e4111aad0e847ba57315efde794f6b07e54
AlyssaU3 <alys4321c@gmail.com>
2025-11-05 13:19:45 -0500

    72 done

--- src/best_record_company.py
+++ src/best_record_company.py
@@ -3,10 +3,15 @@
 import pandas as pd
 
 def best_record_company():
-    pass
-
+    records= pd.read_csv("src/UK-top40-1964-1-2.tsv", sep=("\t"))
+    
+    best= records.groupby("Publisher")["WoC"].sum()
+    best_c=best.idxmax()
+    mask= records["Publisher"]==best_c
+    right_company= records[mask]
+    return right_company
 def main():
-    best_record_company()
+    print(best_record_company())
     
 
 if __name__ == "__main__":
